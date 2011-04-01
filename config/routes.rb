9b47def1dfad53f719a5268bda5c19a8e1b997f9
Matthew Leventi <mleventi@gmail.com>
2011-03-31 23:14:17 -0700

    Initial Post Resource

--- config/routes.rb
+++ config/routes.rb
@@ -48,7 +48,7 @@ Wrtcode::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  root :to => "application#index"
+  root :to => "posts#index"
 
   # See how all your routes lay out with "rake routes"
 
