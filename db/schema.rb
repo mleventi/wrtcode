c7e2222a749f189905152c3c88195f2e769ab881
Matthew Leventi <mleventi@gmail.com>
2011-03-31 23:29:53 -0700

    Give blogs titles

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,13 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20110401055947) do
+ActiveRecord::Schema.define(:version => 20110401062900) do
 
   create_table "posts", :force => true do |t|
     t.text     "content"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "title"
   end
 
 end
