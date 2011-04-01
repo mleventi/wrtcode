bdbc8857da1b1cf528ce1a8ec14c4551108dcb5b
Matthew Leventi <mleventi@gmail.com>
2011-03-31 22:07:48 -0700

    Vanilla Rails

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Wrtcode::Application.config.secret_token = '01c9948c83755f18207e085f6128f162ecbc1e9959d6930b678c7e7c2873139eb18e08387721ab95d906797af8b1c158e616a7e847b69fa794696d39135fef1f'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Wrtcode::Application.config.session_store :cookie_store, :key => '_wrtcode_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Wrtcode::Application.config.session_store :active_record_store
